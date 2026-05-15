3483bf1379d3ba4efc268759c2e77f44174a31ff
das_grablied <solifugae6@yahoo.com>
2026-05-15 16:43:37 -0400

    Updated get_files_info.py to make it list files and updated test_get_files_info.py accordingly

--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,13 +1,24 @@
 from functions.get_files_info import get_files_info
 
-result = get_files_info("calculator", ".")
-print(result)
 
-result = get_files_info("calculator", "pkg")
-print(result)
+def test():
+    result = get_files_info("calculator", ".")
+    print("Result for current directory:")
+    print(result)
+    print("")
 
-result = get_files_info("calculator", "/bin")
-print(result)
+    result = get_files_info("calculator", "pkg")
+    print("Result for 'pkg' directory:")
+    print(result)
 
-result = get_files_info("calculator", "../")
-print(result)
+    result = get_files_info("calculator", "/bin")
+    print("Result for '/bin' directory:")
+    print(result)
+
+    result = get_files_info("calculator", "../")
+    print("Result for '../' directory:")
+    print(result)
+
+
+if __name__ == "__main__":
+    test()
